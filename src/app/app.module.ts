a762f27384194d96c7bcca4263ecdd0cd80cab6d
David Perez Velilla <dperezve@emeal.nttdata.com>
2022-05-11 12:25:56 +0200

    creacion components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AddTeamComponent } from './components/add-team/add-team.component';
+import { EditTeamComponent } from './components/edit-team/edit-team.component';
+import { TeamListComponent } from './components/team-list/team-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTeamComponent,
+    EditTeamComponent,
+    TeamListComponent
   ],
   imports: [
     BrowserModule

fa47aedaa5e200da6ed63d2bf8087a3efdd6f861
David Perez Velilla <dperezve@emeal.nttdata.com>
2022-06-02 17:33:36 +0200

    tests

--- src/app/shared/auth.service.spec.ts
+++ src/app/shared/auth.service.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
-
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+    });
     service = TestBed.inject(AuthService);
   });
 
